09618df1dcb3ebb8f446d4f37a9e065d3e1e3694
varad-pawar1 <varadpawarm@gmail.com>
2024-12-15 23:27:44 +0530

    error solve at lecture

--- Backend/controllers/lectureController.js
+++ Backend/controllers/lectureController.js
@@ -1,12 +1,12 @@
 import { creationData } from "../models/instructorModel.js"
 
-const scheduleStudent = async () => {
+const scheduleStudent = async (req,res) => {
 
     try {
         const data = await creationData.find({})
         res.status(200).json({ data });
     } catch (error) {
-        res.status(500).json({ error, msg: "while feacthing data" });
+        res.status(500).json({ error });
 
     }
 }
